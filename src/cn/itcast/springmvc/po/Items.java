f81673ea49a19e97e44a3eee8247c66675d1c0f2
chencrazy2016 <840693886@qq.com>
2017-07-02 22:37:13 +0800

    测试

---
+++ src/cn/itcast/springmvc/po/Items.java
@@ -0,0 +1,34 @@
+package cn.itcast.springmvc.po;
+
+/**
+ * Created by Administrator on 2017/7/2 0002.
+ */
+public class Items {
+    private String name;
+    private String price;
+    private String detail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
